811eb309b0b92aaaa3184825969c3511ac5d585b
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2023-05-30 13:29:32 -0300

    adding the js file

---
+++ app.js
@@ -0,0 +1,11 @@
+function evenOrOdd(number) {
+//determine if number is odd or even
+  if (number % 2 === 0) {
+//return 'Even' if even
+     return 'Even';
+  }
+//return 'Odd' if anything else (odd, undefined)
+  else {
+     return 'Odd';
+  } 
+}
